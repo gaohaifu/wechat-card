2836f5182ab3a49f7e7776578d2162343368c728
qiu <>
2024-01-27 17:06:04 +0800

    1

--- addons/smartcard/config.php
+++ addons/smartcard/config.php
@@ -12,8 +12,8 @@ return [
             'scope' => 'snsapi_base',
         ],
         'value' => [
-            'app_id' => 'wx***f8',
-            'app_secret' => '8*****3d',
+            'app_id' => 'wx4a20e5d0412ab4eb',
+            'app_secret' => 'a7333748f761a4a60ac5753c09c937e2',
             'scope' => 'snsapi_userinfo',
         ],
         'rule' => 'required',
